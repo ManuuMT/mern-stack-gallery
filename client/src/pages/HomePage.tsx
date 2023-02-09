0f7ea0712219be07d3c3c1b9274e15904122b450
Emanuel Maldonado <ema14mt@gmail.com>
2023-02-09 02:48:38 -0300

    Tailwind installed

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,9 @@
 const HomePage: React.FC = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="w-screen min-h-screen flex items-center justify-center">
+      <h1>HomePage</h1>
+    </div>
+  );
 };
 
 export default HomePage;
