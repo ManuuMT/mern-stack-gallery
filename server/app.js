6c22e03f02ee52360112ec771d3605477cce9fe9
Emanuel Maldonado <ema14mt@gmail.com>
2023-02-14 19:50:20 -0300

    Deploy configurations added

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import postsRoutes from "./routes/posts.routes.js";
 import fileupload from "express-fileupload";
 import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // middlewares
 app.use(cors());
@@ -17,5 +20,6 @@ app.use(
 
 // routes
 app.use(postsRoutes);
+app.use(express.static(join(__dirname, "../client/dist")));
 
 export default app;
